427903e5b14bbe1a72cf89f085dff9b39a3a4e15
usama0011 <za5232208@gmail.com>
2021-10-14 18:14:16 +0500

    day one build

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Sidebar from 'components/Sidebar';
+import Dashboard from 'pages/Dashboard';
+import Settings from 'pages/Settings';
+import Tables from 'pages/Tables';
+import Maps from 'pages/Maps';
+import Footer from 'components/Footer';
+
+// Tailwind CSS Style Sheet
+import 'assets/styles/tailwind.css';
+import Login from 'components/Login';
+
+function App() {
+    return (
+        <>
+            <Sidebar />
+            <div className="md:ml-64">
+                <Switch>
+                    <Route exact path="/" component={Dashboard} />
+                    <Route exact path="/settings" component={Settings} />
+                    <Route exact path="/tables" component={Tables} />
+                    <Route exact path="/maps" component={Maps} />
+                    <Route exact path="/login" component={Login} />
+                    <Redirect from="*" to="/" />
+                </Switch>
+                <Footer />
+            </div>
+        </>
+    );
+}
+
+export default App;
